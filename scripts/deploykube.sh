7df97ff63d1598cc6dfcf73ee029693d9cdc7420
Ubuntu <ubuntu@ip-172-31-33-11.eu-west-1.compute.internal>
2020-11-21 22:09:11 +0000

    jenkinstest

--- scripts/deploykube.sh
+++ scripts/deploykube.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cd kube
+kubectl create ns pipeline
 kubectl apply -f nginx-config.yaml -n pipeline
 kubectl apply -f secret.yaml -n pipeline
 kubectl apply -f backend.yaml -f frontend.yaml -f nginx.yaml
-kubectl describe service/nginx-loadbalancer -n pipeline
+kubectl describe service/nginx-lb -n pipeline
--- scripts/testscript.sh
+++ scripts/testscript.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 
 ssh ubuntu@${TEST_IP}
-git clone https://github.com/AAtkinsonQA/CI-Pipeline.git
+git clone --branch kubernetes https://github.com/AAtkinsonQA/CI-Pipeline.git
 cd CI-Pipeline
-sudo -E DATABASE_URI=${DATABASE_URI} -E TEST_DATABASE_URI=${TEST_DATABASE_URI} -E SECRET_KEY=${SECRET_KEY} docker-compose up -d
+sudo docker login --username=${DOCKER_USER} --password=${DOCKER_PASSWORD}
+sudo -E TEST_DATABASE_URI=${TEST_DATABASE_URI} -E SECRET_KEY=${SECRET_KEY} docker-compose up -d
 echo "Running Tests: "
 sudo docker exec frontend bash -c "pytest tests/ --cov application"
 sudo docker exec backend bash -c "pytest tests/ --cov application"
-docker-compose down
+sudo docker-compose down
 cd ..
 rm -rf CI-Pipeline
 ls
