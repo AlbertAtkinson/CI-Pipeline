323bf6df778d549e6401331b49e950be5233211e
Ubuntu <ubuntu@ip-172-31-33-11.eu-west-1.compute.internal>
2020-11-21 22:17:21 +0000

    chmod scripts

--- scripts/dockerhub.sh
+++ scripts/dockerhub.sh
@@ -5,5 +5,6 @@ cd ..
 cd backend
 sudo docker build -t aatkinson42/back-end .
 cd ..
+sudo docker login --username=${DOCKER_USER} --password=${DOCKER_PASSWORD}
 sudo docker push aatkinson42/back-end
 sudo docker push aatkinson42/front-end
--- scripts/testscript.sh
+++ scripts/testscript.sh
@@ -3,7 +3,6 @@
 ssh ubuntu@${TEST_IP}
 git clone --branch kubernetes https://github.com/AAtkinsonQA/CI-Pipeline.git
 cd CI-Pipeline
-sudo docker login --username=${DOCKER_USER} --password=${DOCKER_PASSWORD}
 sudo -E TEST_DATABASE_URI=${TEST_DATABASE_URI} -E SECRET_KEY=${SECRET_KEY} docker-compose up -d
 echo "Running Tests: "
 sudo docker exec frontend bash -c "pytest tests/ --cov application"
