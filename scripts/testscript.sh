6d493e57a3f7f260e76750af8df2bf45886e0479
Ubuntu <ubuntu@ip-172-31-33-11.eu-west-1.compute.internal>
2020-11-21 22:52:23 +0000

    ssh command

--- scripts/testscript.sh
+++ scripts/testscript.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-ssh ubuntu@${TEST_IP}
 git clone --branch jenkinstest https://github.com/AAtkinsonQA/CI-Pipeline.git
 cd CI-Pipeline
 sudo -E TEST_DATABASE_URI=${TEST_DATABASE_URI} -E SECRET_KEY=${SECRET_KEY} docker-compose up -d
