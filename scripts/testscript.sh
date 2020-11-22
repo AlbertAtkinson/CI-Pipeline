4846545d39e517d290b58feb4ee96e5caf6e80e0
Ubuntu <ubuntu@ip-172-31-33-11.eu-west-1.compute.internal>
2020-11-22 21:58:36 +0000

    security

--- scripts/testscript.sh
+++ scripts/testscript.sh
@@ -3,8 +3,8 @@
 sudo rm -r CI-Pipeline
 git clone --branch testconf https://github.com/AAtkinsonQA/CI-Pipeline.git
 cd CI-Pipeline
-mysql -h k8s-rds.cbf9oalhjjnm.eu-west-1.rds.amazonaws.com -P 3306 -u root -ppass5678 < database/Create.sql
-mysql -h test-rds.cbf9oalhjjnm.eu-west-1.rds.amazonaws.com -P 3306 -u root -ppass1234 < database/Create.sql
+mysql -h k8s-rds.cbf9oalhjjnm.eu-west-1.rds.amazonaws.com -P 3306 -u {USERNAME} -p{k8sPASSWORD} < database/Create.sql
+mysql -h test-rds.cbf9oalhjjnm.eu-west-1.rds.amazonaws.com -P 3306 -u {USERNAME} -p{testPASSWORD} < database/Create.sql
 sudo docker-compose up -d --build
 echo "Running Tests: "
 sudo docker exec frontend bash -c "pytest tests/ --cov application"
