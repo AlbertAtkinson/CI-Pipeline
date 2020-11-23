5f389979b8e8837f9ac06a413b7947646a714783
Ubuntu <ubuntu@ip-172-31-33-11.eu-west-1.compute.internal>
2020-11-23 10:56:44 +0000

    testing without security

--- scripts/dockerhub.sh
+++ scripts/dockerhub.sh
@@ -5,6 +5,6 @@ cd ..
 cd backend
 sudo docker build -t aatkinson42/back-end .
 cd ..
-sudo docker login --username={DOCKER_USER} --password={DOCKER_PASS}
+sudo docker login --username="aatkinson42" --password="fredCoding442%"
 sudo docker push aatkinson42/back-end
 sudo docker push aatkinson42/front-end
