bd5ed4930f69181f74ef9874120f650d43689f6e
Ubuntu <ubuntu@ip-172-31-33-11.eu-west-1.compute.internal>
2020-11-23 14:36:14 +0000

    test

--- scripts/deploykube.sh
+++ scripts/deploykube.sh
@@ -6,4 +6,3 @@ kubectl apply -f nginx-config.yaml -n pipeline
 kubectl apply -f secret.yaml -n pipeline
 kubectl apply -f backend.yaml -f frontend.yaml -f nginx.yaml
 kubectl describe service/nginx-lb -n pipeline
-#test
